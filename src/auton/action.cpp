4672b2dd7c178f83bddee089a7ea05cd55768f1a
meisZWFLZ <80860310+meisZWFLZ@users.noreply.github.com>
2022-10-28 12:10:26 -0700

    school code

--- src/auton/action.cpp
+++ src/auton/action.cpp
@@ -16,29 +16,32 @@ inline constexpr float headingDifference(float a, float b) {
 }
 void auton::GoTo::execute() const {
   static const float pidLimit = 50;
-  while (abs(Position::distance({0,0}, Robot::getPosition())) > 0.1) {
+  Position robotPos = Robot::getPosition();
+  float distance = Position::distance({0,0}, Robot::getPosition());
+   
+  while (distance > 0.1) {
     if (Controller1.ButtonA.pressing())
       break;
-
-    const float distance = Position::distance({0,0}, Robot::getPosition());
-    const Position vector = Position::difference({0,0,0}, Robot::getPosition());
+    
+   
+    const Position vector = Position::difference({0,0,0}, robotPos);
     printf(")distance:%f\n", distance);
     const float target1 = atan2(vector.x, vector.y);
     const float target = target1 < 0 ? target1 + pi2 : target1;
     const float heading1 =
         Conversions::Degrees::toRadians(Inertial10.heading(degrees));
     const float headingDiff = headingDifference(target, heading1) /*  * 100 */;
-    printf("{heading: %f\n+diff: %f\n@target: %f\n&input: %f\n", heading1,
-           headingDiff /*  / 100 */, target, 1);
+   
+    printf("{heading: %f\n+diff: %f\n@target: %f\n", heading1,
+           headingDiff /*  / 100 */, target);
     const float stopAdjust =
         pow(cos(headingDiff), 5) * 40 * distance / (2 + abs(distance));
-    const float turnAdjust = (20 * headingDiff / (0.5 + abs(heading1)));
+    const float turnAdjust = (20 * headingDiff / (0.5 + abs(headingDiff)));
 
     const float leftAdjustment = stopAdjust - turnAdjust;
     const float rightAdjustment = stopAdjust + turnAdjust;
 
-    printf("(%f,%f,%f)", Robot::getPosition().x, Robot::getPosition().y,
-           Robot::getPosition().heading);
+    printf("(%f,%f,%f)", robotPos.x, robotPos.y, robotPos.heading);
 
         if (abs(leftAdjustment) < pidLimit)
             Robot::Drivetrain::left(leftAdjustment);
@@ -52,19 +55,22 @@ void auton::GoTo::execute() const {
       printf("right at limit: %f\n", rightAdjustment);
       Robot::Drivetrain::right(rightAdjustment > 0 ? pidLimit : -pidLimit);
     };
+    
     Controller1.Screen.setCursor(0, 0);
-
-    static Position robotPos1 = Robot::getPosition();
     Controller1.Screen.clearLine();
     Controller1.Screen.print("(");
-    Controller1.Screen.print(robotPos1.x);
+    Controller1.Screen.print(robotPos.x);
     Controller1.Screen.print(",");
-    Controller1.Screen.print(robotPos1.y);
+    Controller1.Screen.print(robotPos.y);
     Controller1.Screen.print(",");
-    Controller1.Screen.print(robotPos1.heading);
+    Controller1.Screen.print(robotPos.heading);
     Controller1.Screen.print(")");
     Controller1.Screen.newLine();
+    
     wait(20, msec);
+    
+    robotPos = Robot::getPosition();
+    distance = Position::distance({0,0}, robotPos);
   }
 };
 auton::GoTo::GoTo(const Position target) : target(target){};
